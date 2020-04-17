2657a95e389606cfcc2970e41a0f0f20651decbe
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-04-17 17:39:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project purpose 
+
+Removing spam
+
+
+# Description
+
+To protests against the lies WHO spreads, we are willing to not be not informed.
---
+++ peaceplease_ff/peaceapp.js
@@ -0,0 +1,5 @@
+addEventListener("load", function() {
+    setTimeout(function() {
+        document.querySelector('#clarify-box').style.display = 'None';
+    }, 0);    
+});
